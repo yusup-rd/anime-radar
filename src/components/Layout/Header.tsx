380b2d8fc4a0956065f86599ff266551a85ae1ff
Yusup <yusuprd@icloud.com>
2025-11-09 00:43:34 +0800

    Updated header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,17 +1,23 @@
 import { useNavigate } from 'react-router-dom';
+import Search from './Search';
 
 const Header = () => {
   const navigate = useNavigate();
 
   return (
     <div className="h-20 bg-[#11111B]">
-      <div className="container flex h-full items-center gap-3">
+      <div className="container flex h-full items-center justify-between gap-12">
         <div
           className="flex cursor-pointer items-center gap-3"
           onClick={() => navigate('/')}
         >
           <img src="/anime.ico" alt="AnimeRadar Logo" className="size-8" />
-          <h1 className="text-primary text-2xl font-semibold">AnimeRadar</h1>
+          <h1 className="text-primary hidden text-2xl font-semibold md:block">
+            AnimeRadar
+          </h1>
+        </div>
+        <div className="max-w-md flex-1">
+          <Search />
         </div>
       </div>
     </div>
