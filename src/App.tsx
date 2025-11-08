4b7cbc5b5a5e54769056cf0fa26428eebe9ec47c
Yusup <yusuprd@icloud.com>
2025-11-08 21:52:34 +0800

    Updated prettier rules and files accordingly

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import DetailPage from './pages/DetailPage'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import DetailPage from './pages/DetailPage';
+import './App.css';
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
         <Route path="/anime/:id" element={<DetailPage />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
