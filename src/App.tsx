01b20474e201eb4f02d6a947a0d59032fa677427
Yusup <yusuprd@icloud.com>
2025-11-08 22:57:40 +0800

    Udpated layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import DetailPage from './pages/DetailPage';
+import Header from './components/Layout/Header';
+import Footer from './components/Layout/Footer';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/anime/:id" element={<DetailPage />} />
-      </Routes>
+      <div className="flex min-h-screen flex-col">
+        <Header />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/anime/:id" element={<DetailPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
   );
 }
